b6a0f797ac69267f600f06a7c60bcfdb4ced2901
Satvik <satviksingh1095@gmail.com>
2023-12-23 16:53:42 +0530

    2nd commit

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,5 @@
 from application import app
-from flask import redirect, render_template, flash, request
+from flask import redirect, render_template, flash, request, url_for
 from .forms import TodoForm
 from flask.templating import render_template_string
 from werkzeug.datastructures import RequestCacheControl
@@ -46,7 +46,7 @@ def add_todo():
         form = TodoForm()
     return render_template("add_todo.html", form=form)
 
-@app.route("/update_todo/<id>", method = ["POST", "GET"])
+@app.route("/update_todo/<id>", methods = ["POST", "GET"])
 def update_todo(id):
     if request.method == "POST":
         form = TodoForm(request.form)
